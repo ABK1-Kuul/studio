acae9e783332d09e012741aa9be507a8c0a8fb33
Abk Kuul <abk1.kul@gmail.com>
2025-05-21 11:25:14 +0000

    in the header remove the space between the X and perts

--- src/components/layout/StyledXperts.tsx
+++ src/components/layout/StyledXperts.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 export function StyledXperts() {
   return (
     <>
-      <span 
-        className="text-accent font-extrabold text-[1.1em] relative" 
+      <span
+        className="text-accent font-extrabold text-[1.1em] relative"
         style={{ top: '-0.05em' }}
       >
         X
@@ -14,4 +14,3 @@ export function StyledXperts() {
     </>
   );
 }
-
